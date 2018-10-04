a4870005a0be4fe43f8289c564179c48022afc9a
Cris Aian Vergara <The21stKuruno@yahoo.com>
2018-10-04 23:00:39 +0900

    added some social links

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,42 @@
 $(function() {
-  // setBindings();
-  // setGoToTop();
+  setGoToTop();
   setDropNav();
 });
+
 function setDropNav() {
   $(".icon").click(function() {
+    $(".icon").toggleClass("active");
     $("#dropbar").toggleClass("visible");
   });
 
-  $("section, .main, footer").click(function() {
+  $("section, footer, .main").click(function() {
+    $(".icon").removeClass("active");
     $("#dropbar").removeClass("visible");
   });
 
   $(window).resize(function() {
+    $(".icon").removeClass("active");
     $("#dropbar").removeClass("visible");
+  });
+}
+
+// Got to top Button
+function setGoToTop() {
+  var offset = 320;
+  var duration = 800;
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > offset) {
+      $("#go-top").fadeIn(duration);
+      $(".fade-me").fadeIn(duration);
+    } else {
+      $("#go-top").fadeOut(duration);
+    }
+  });
+
+  // Click event to scroll to top
+  $("#go-top").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, 800);
+    return false;
   });
 }
